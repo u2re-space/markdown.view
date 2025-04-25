03fc1aca7af338525468c8abec1386d2ac0062f5
u2re-dev <c24b@u2re.ru>
2025-04-25 17:10:44 +0700

    fix dirs

--- main.mjs
+++ main.mjs
@@ -2,8 +2,8 @@
 import "./scss/markdown/github-markdown-dark.scss"
 import "./scss/markdown/github-markdown-light.scss"
 import "./scss/print/Print.scss"*/
-import "./scss/main.scss"
-import "./js/Markdown.mjs"
+import "./src/scss/main.scss"
+import "./src/js/Markdown.mjs"
 
 //
 addEventListener("message", (ev)=>{
